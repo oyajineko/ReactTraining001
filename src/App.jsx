7ede29cb4dbbcbc5c7ba6b69bcd0b1e6c0fbbeef
oyajineko <75714902+oyajineko@users.noreply.github.com>
2020-12-15 14:31:58 +0900

    test test

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,13 @@ import React from "react";
 
 const App = () => {
   const onClickButton = () => alert("ueda baka");
+  const contentStyle = {
+    color: "blue",
+    fontSize: "18px"
+  };
   return (
     <>
-      <h1>こんにちは！</h1>
+      <h1 style={contentStyle}>こんにちは！</h1>
       <p>お元気ですか？</p>
       <button onClick={onClickButton}>ボタン</button>
     </>
